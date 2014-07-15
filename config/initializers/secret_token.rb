d526b06b4a4a35df851bed70bc7e6898fc931c2c
jeffrey.yang <jeffrey.yang@oceanwing.com>
2014-07-15 15:50:42 +0800

    reinit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodFirst::Application.config.secret_key_base = '940ea446bcf107f5935fd49c1eb4fd9265cd6c443d212c5a6ba766caf89f93ad575bbf28ab459521b0e0ab0dae5fdff38c0f972898b76a83f0346601233c45ef'
